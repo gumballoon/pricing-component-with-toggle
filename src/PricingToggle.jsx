0e5c005e6332cc67f82a3162ef059bfb82904223
Francisco Cristina <franciscocristina.sound@gmail.com>
2025-08-14 11:58:08 +0100

    final tweaks to complete the publishing

--- src/PricingToggle.jsx
+++ src/PricingToggle.jsx
@@ -14,7 +14,8 @@ function PricingToggle({ mode, setMode }) {
             <span className="gray-650 opacity-50">Annually</span>
 
             <div className="form-check form-switch mx-4 my-0 p-0">
-                <input className="form-check-input shadow-none m-0 toggle" type="checkbox" role="switch" checked={mode === 'monthly' ? true : false} onChange={handleChange}/>
+                <input id="billing-toggle" className="form-check-input shadow-none m-0 toggle" type="checkbox" role="switch" checked={mode === 'monthly' ? true : false} onChange={handleChange} aria-label="Toggle between monthly and annual billing"/>
+                <label htmlFor="billing-toggle" className="visually-hidden">Toggle between monthly and annual billing</label>
             </div>
 
             <span className="gray-650 opacity-50">Monthly</span>
